698390467a2f6bdae5c6ed032de4f712e7a1ef38
precis308acs <precis308acs@gmail.com>
2018-06-07 15:25:47 +0300

    Add qr codes to rviz

--- src/positions_getter.py
+++ src/positions_getter.py
@@ -1,5 +1,8 @@
-import socket
 import sys
+task_path = "../../hall-moving-scenario/"
+sys.path.insert(0, task_path)
+
+import socket
 import pickle
 import numpy as np
 import collections
@@ -21,10 +24,10 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from geometry_msgs.msg import Quaternion, Pose, Point, Vector3
 from std_msgs.msg import Header, ColorRGBA
+from TaskManagement.tasksManagement import Task
 
 #keystrokes
 import select
-
 
 
 IP = "192.168.0.158"
@@ -36,13 +39,15 @@ class TcpClient:
 		self.ip = IP
 		self.port = PORT
 		self.person_positions = {}
+		self.location_positions = {}
 		self.connected = False
 		self.map_pose = None
 		rospy.init_node('talker', anonymous=True)
 		self.opened_connection = True
 		self.navigation = PepperNavigation()
-		self.marker_array_publisher = rospy.Publisher('/people', MarkerArray, queue_size=100)
-	
+		self.people_marke_array_publisher = rospy.Publisher('/people', MarkerArray, queue_size=100)
+		self.locations_marker_array_publisher = rospy.Publisher('/qrCodes', MarkerArray, queue_size=100)
+
 	def connect(self):
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
@@ -66,41 +71,79 @@ class TcpClient:
 		else:
 			return False
 
+	def publish_persons(self):
+		markers = []
+		i = 0
+		for person_name in self.person_positions:
+			from copy import deepcopy
+			marker = Marker(
+				type=Marker.TEXT_VIEW_FACING,
+				id=i,
+				lifetime=rospy.Duration(2.0),
+				pose=deepcopy(self.person_positions[person_name][0].pose),
+				scale=Vector3(0.2, 0.2, 0.2),
+				header=Header(frame_id='odom'),
+				color=ColorRGBA(0.0, 1.0, 0.0, 0.8),
+				text=person_name)
+
+			marker.pose.position.z += 0.3
+			markers.append(marker)
+			i += 1
+			marker2 = Marker(
+				type=Marker.SPHERE,
+				id=i,
+				lifetime=rospy.Duration(2.0),
+				pose=self.person_positions[person_name][0].pose,
+				scale=Vector3(0.2, 0.2, 0.2),
+				header=Header(frame_id='odom'),
+				color=ColorRGBA(0.0, 1.0, 0.0, 0.8),
+				text=person_name)
+			markers.append(marker2)
+			i += 1
+		self.people_marke_array_publisher.publish(markers)
+
+	def publish_locations(self):
+		markers = []
+		i = 0
+		for location in self.location_positions:
+			from copy import deepcopy
+			marker = Marker(
+				type=Marker.TEXT_VIEW_FACING,
+				id=i,
+				lifetime=rospy.Duration(2.0),
+				pose=deepcopy(self.location_positions[location][0].pose),
+				scale=Vector3(0.2, 0.2, 0.2),
+				header=Header(frame_id='odom'),
+				color=ColorRGBA(0.0, 0.0, 1.0, 0.8),
+				text=location)
+
+			marker.pose.position.z += 0.3
+			markers.append(marker)
+			i += 1
+			marker2 = Marker(
+				type=Marker.SPHERE,
+				id=i,
+				lifetime=rospy.Duration(2.0),
+				pose=self.location_positions[location][0].pose,
+				scale=Vector3(0.2, 0.2, 0.2),
+				header=Header(frame_id='odom'),
+				color=ColorRGBA(0.0, 0.0, 1.0, 0.8),
+				text=location)
+			markers.append(marker2)
+			i += 1
+		self.locations_marker_array_publisher.publish(markers)
+
 	def get_data(self):
 		try:
 			while self.connected:
-				data = pickle.loads(self.socket.recv(BUFFER_SIZE))
+				raw_data = self.socket.recv(BUFFER_SIZE)
+				try:
+					data = pickle.loads(raw_data)
+				except:
+					continue
 				self.parse_data(data)
-				i = 0
-				markers = []
-				for person_name in self.person_positions:
-					from copy import deepcopy
-					marker = Marker(
-						type=Marker.TEXT_VIEW_FACING,
-						id=i,
-						lifetime=rospy.Duration(2.0),
-						pose=deepcopy(self.person_positions[person_name][0].pose),
-						scale=Vector3(0.2, 0.2, 0.2),
-						header=Header(frame_id='odom'),
-						color=ColorRGBA(0.0, 1.0, 0.0, 0.8),
-						text=person_name)
-
-					marker.pose.position.z += 0.3
-					markers.append(marker)
-					i += 1
-					marker2 = Marker(
-						type=Marker.SPHERE,
-						id=i,
-						lifetime=rospy.Duration(2.0),
-						pose=self.person_positions[person_name][0].pose,
-						scale=Vector3(0.2, 0.2, 0.2),
-						header=Header(frame_id='odom'),
-						color=ColorRGBA(0.0, 1.0, 0.0, 0.8),
-						text=person_name)
-					markers.append(marker2)
-					i += 1
-				self.marker_array_publisher.publish(markers)
-
+				self.publish_persons()
+				self.publish_locations()
 
 		except KeyboardInterrupt:
 			print("Script interrupted by user, shutting down.")
@@ -117,42 +160,41 @@ class TcpClient:
 		if not data or len(data) == 0:
 			return
 
-		for person in data:
-			pos_in_laser = [person[1][0], person[1][1], person[1][2]]
-			br = tf.TransformBroadcaster()
-			br.sendTransform(pos_in_laser,
-							 tf.transformations.quaternion_from_euler(0, 0, 0),
-							 rospy.Time.now(),
-							 'human_in_laser',
-							 'laser')
-			pose = PoseStamped()
-			loc = Pose()
-			robot_pos = self.navigation.get_pose().pose
-			loc.position.x = pos_in_laser[0]
-			loc.position.y = pos_in_laser[1]
-			loc.position.z = pos_in_laser[2]
-			loc.orientation = robot_pos.orientation
-			pose.header.frame_id = 'laser'
-			pose.pose = loc
+		for task in data:
+			if task.type == 'show_on_map':
+				pose = PoseStamped()
+				loc = Pose()
+				robot_pos = self.navigation.get_pose().pose
+				loc.position.x = task.coordinates[0]
+				loc.position.y = task.coordinates[1]
+				loc.position.z = task.coordinates[2]
+				loc.orientation = robot_pos.orientation
+				pose.header.frame_id = 'laser'
+				pose.pose = loc
 
-			listener = tf.TransformListener()
-			try:
-				listener.waitForTransform("/laser", "/map", rospy.Time(), rospy.Duration(4.0))
-				pose_in_map = listener.transformPose('/odom', pose)
-				pose_in_map.header.frame_id = 'odom'
+				listener = tf.TransformListener()
+				try:
+					listener.waitForTransform("/laser", "/map", rospy.Time(), rospy.Duration(4.0))
+					pose_in_map = listener.transformPose('/odom', pose)
+					pose_in_map.header.frame_id = 'odom'
 
-				if len(person) > 2:
-					person_name = person[2]
-				else:
-					person_name = "unknown"
+					if task.person_name != '':
+						person_name = task.person_name
+						if not person_name in self.person_positions:
+							self.person_positions[person_name] = [pose_in_map]
+							print("Found " + person_name)
+						else:
+							self.person_positions[person_name][0] = pose_in_map
 
-				if not person_name in self.person_positions:
-					self.person_positions[person_name] = [pose_in_map]
-					print("Found " + person_name)
-				else:
-					self.person_positions[person_name][0] = pose_in_map
-			except:
-				pass
+					if task.location != '':
+						location = task.location
+						if not location in self.location_positions:
+							self.location_positions[location] = [pose_in_map]
+							print("Found " + location)
+						else:
+							self.location_positions[location][0] = pose_in_map
+				except:
+					pass
 
 if __name__ == "__main__":
 	from threading import Thread
